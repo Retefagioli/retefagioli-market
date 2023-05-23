55a705f78b80d5369d62085cb78ea596f9e4b22e
Blessium <alessandro.melgar04@gmail.com>
2023-05-23 20:39:48 +0200

    Added base project

---
+++ src/main/java/io/retefagioli/retefagiolimarket/RetefagioliMarketApplication.java
@@ -0,0 +1,13 @@
+package io.retefagioli.retefagiolimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetefagioliMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetefagioliMarketApplication.class, args);
+	}
+
+}
